15da87ba538b618b3704b04e685d67a871b7ac77
KvTsira <kvtsira@gmail.com>
2022-04-03 22:13:28 -0400

    adding text formatting to helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,19 @@
-
 module.exports = {
-    format_plural: (word, amount) => {
-        if (amount !== 1) {
-            return `${word}s`
-        }
-        return `${word}`
-    },
-    format_date: date => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-            date
-        ).getFullYear()}`;
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
     }
-}
+    return `${word}`;
+  },
+  format_post: (text) => {
+    if (text.length > 150) {
+      return text.substring(0, 150) + "...";
+    }
+    return text;
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+};
